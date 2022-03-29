cddace2c00d7714d2d7eb24900870b1183769fad
flex <geroi_cool@mail.ru>
2022-03-29 18:51:02 +0300

    8 дз

--- todoapp/tests.py
+++ todoapp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from mixer.backend.django import mixer
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APITestCase
 
-# Create your tests here.
+from todoapp.views import ProjectModelViewSet
+from usersapp.models import Users
+
+
+class TestAPI(TestCase):
+    def setUp(self) -> None:
+        self.admin = Users.objects.create_superuser(username='admin', email='admin@example.com', password='admin')
+
+    def test_projects_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/projects/')
+        force_authenticate(request, self.admin)
+        view = ProjectModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), 0)
+
+    def test_todos_list(self):
+        client = APIClient()
+        response = client.get('/api/todos/')
+        self.assertEqual(response.status_code, 401)
+        client.login(username='admin', password='admin')
+        response = client.get('/api/todos/')
+        self.assertEqual(response.status_code, 200)
--- usersapp/tests.py
+++ usersapp/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from mixer.backend.django import mixer
+from usersapp.models import Users
 
-# Create your tests here.
+
+class TestAPI(APITestCase):
+    def setUp(self) -> None:
+        self.admin = Users.objects.create_superuser(username='admin', email='admin@example.com', password='admin')
+        mixer.cycle(5).blend(Users)
+
+    def test_users_list(self):
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.get('/api/users/')
+        self.assertEqual(len(response.data['results']), 6)
